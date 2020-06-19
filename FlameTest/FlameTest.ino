47a26bc8b48a6d79718d06aebe3a03247c3081d8
sreeky <36283333+kanthian@users.noreply.github.com>
2020-06-18 20:46:20 -0700

    serial logging and pin change

--- FlameTest/FlameTest.ino
+++ FlameTest/FlameTest.ino
@@ -7,15 +7,19 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pixels.begin();
+  Serial.begin(57600);
 }
 void loop() {
 
   pixels.clear(); // Set all pixel colors to 'off'
   for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(255, 192, 203))//Pink
+    pixels.setPixelColor(i, pixels.Color(255, 100, 180))//Pink
     ;
     pixels.setBrightness(100);
     pixels.show();
+  Serial.print("loop");
+  Serial.println(i);
+  //Serial.println("string");
     delay(DELAYVAL);
   }
 }
