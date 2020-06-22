1b2264544e32304a0ffe3a38f0cfd2487719fdc1
Shane Crosby <croz@google.com>
2020-06-21 19:52:28 -0700

    Adding in voltage / current limiter

--- croz-example/croz_test/croz_test.ino
+++ croz-example/croz_test/croz_test.ino
@@ -7,16 +7,17 @@
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2813
 #define NUM_LEDS    144
-#define BRIGHTNESS  10
 #define FRAMES_PER_SECOND 60
-
 #define COOLING  55
 #define SPARKING 120
+
+// This gets nerfed by the call to setMaxPowerInVoltsAndMilliamps below
+#define BRIGHTNESS  255
 
 
 // LED Data
 bool gReverseDirection = false;
-CRGB leds[NUM_LEDS]; 
+CRGB leds[NUM_LEDS];
 CRGBPalette16 gPal;
 
 // Set the delay between BNO055 samples
@@ -29,16 +30,16 @@ void setup() {
   // Initialize I2C library
   Wire.begin();
 
-  // Start up the serial interface  
+  // Start up the serial interface
   Serial.begin(9600);
   while (!Serial); // Wait for Serial
   Serial.println("Starting...");
-  
+
   // Print out the list of I2C devices that are discovered
   debugI2C();
 
   // Try to start up the BNO055, resetting it if it does not start as expected
-  if(bno.begin()) {
+  if (bno.begin()) {
     Serial.println("BNO055 initialized successfully\n");
   } else {
     while (!bno.begin()) {
@@ -52,14 +53,18 @@ void setup() {
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness( BRIGHTNESS );
   gPal = CRGBPalette16( CRGB::Black, CRGB::LightPink, CRGB::Pink,  CRGB::HotPink);
+  
+  // Adjust this to limit the amount of current flowing through Teensy
+  // Up this from 250 when connecting directly to the battery pack
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 250);
 }
 
 void loop() {
   random16_add_entropy( random());
-  
+
   sensors_event_t event;
   bno.getEvent(&event);
-  
+
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
   Serial.print("\tY: ");
@@ -69,7 +74,7 @@ void loop() {
   Serial.println("");
   
   FastLED.setBrightness( event.orientation.y / 3 );
-  
+
   Fire2012WithPalette();
   FastLED.show();
   delay(1000 / FRAMES_PER_SECOND);
@@ -117,36 +122,36 @@ void Fire2012WithPalette() {
 // the resetBNO055() function can be used as long as it is wired correctly.
 // Stolen from i2c_scanner on Ardiuno Playground.
 //
-// Good: I2C device found at address 0x28 
+// Good: I2C device found at address 0x28
 // Bad: I2C device found at address 0x40
 void debugI2C() {
   byte error, address;
   int nDevices;
- 
+
   Serial.println("Scanning for I2C devices");
- 
+
   nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    // The i2c_scanner uses the return value of   
+  for (address = 1; address < 127; address++ ) {
+    // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
- 
+
     if (error == 0) {
       Serial.print("I2C device found at address 0x");
-      if (address<16)
+      if (address < 16)
         Serial.print("0");
-      Serial.print(address,HEX);
+      Serial.print(address, HEX);
       Serial.println("  !");
       nDevices++;
     }
-    else if (error==4) {
+    else if (error == 4) {
       Serial.print("Unknown error at address 0x");
-      if (address<16)
+      if (address < 16)
         Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+      Serial.println(address, HEX);
+    }
   }
   if (nDevices == 0) {
     Serial.println("No I2C devices found\n");
@@ -158,7 +163,7 @@ void debugI2C() {
 //   PS0 -> GND
 //   PS1 -> GND
 //   RST -> Teensy PIN 7
-void resetBNO055() {    
+void resetBNO055() {
   bool broken = true;
   pinMode(7, OUTPUT);
 
